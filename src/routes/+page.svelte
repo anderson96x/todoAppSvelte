f13bba85319ae90974b7c962b714b2780b081d16
Anderson Lobo <92031131@DGTI-DESENV10.pcivil.intranet>
2025-08-12 17:56:49 -0300

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { Button, Input } from 'flowbite-svelte';
+
 	let todos: any = [];
 
 	let inputValue = '';
@@ -14,14 +16,24 @@
 	};
 </script>
 
-<form on:submit={handleSubmit}>
-	<input type="text" bind:value={inputValue} />
-	<button>Add</button>
-</form>
+<div class="mt-15 h-full w-full justify-items-center">
+	<h1 class="m-5 text-8xl text-white drop-shadow-lg" style="font-family: 'Ephesis'">To-do list</h1>
 
-{#each todos as todo}
-	<div class="flex flex-row">
-		<div>{todo.todoText}</div>
-		<button on:click={() => handleDelete(todo.Id)}>X</button>
-	</div>
-{/each}
+	<form on:submit={handleSubmit}>
+		<div class="flex w-100 flex-row">
+			<Input type="text" bind:value={inputValue} />
+			<Button class="ms-3" type="submit" color="blue">Add</Button>
+		</div>
+	</form>
+
+	{#each todos as todo}
+		<div
+			class="m-3 flex w-100 flex-row items-center rounded-lg border border-gray-300 bg-gray-50 p-3 text-gray-900 drop-shadow-md dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+		>
+			<div class="break-word w-90">
+				{todo.todoText}
+			</div>
+			<Button class="h-10 w-10" color="red" onclick={() => handleDelete(todo.Id)}>X</Button>
+		</div>
+	{/each}
+</div>
