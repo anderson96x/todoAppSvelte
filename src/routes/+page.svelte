6bb59bcc522a08b78a9353b1616873dc53462e95
Anderson Lobo <92031131@DGTI-DESENV10.pcivil.intranet>
2025-08-12 12:18:06 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let todos: any = [];
+
+	let inputValue = '';
+	let handleSubmit = () => {
+		todos = [...todos];
+		todos.push({todoText: inputValue, Id: crypto.randomUUID()});
+        console.log(todos);
+	};
+</script>
+
+<form on:submit={handleSubmit}>
+	<input type="text" bind:value={inputValue} />
+	<button>Add</button>
+</form>
+
+{#each todos as todo}
+	<p>{todo.todoText}</p>
+{/each}
